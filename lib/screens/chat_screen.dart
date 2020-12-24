aaecf74912f334f4c315c14026889caf8298dded
juxs salley <juxsalley@gmail.com>
2020-12-24 18:33:27 +0000

    flash app

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flash_chat/constants.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
 
 class ChatScreen extends StatefulWidget {
   static const String id = 'chatScreen';
@@ -18,12 +19,12 @@ class _ChatScreenState extends State<ChatScreen> {
   Future<void> getCurrentUser() async {
     var currentUser = await firebaseAuth.currentUser();
     if (currentUser != null) {
-      user = currentUser;
+      user = await currentUser;
       print(user.email);
     }
   }
 
-  void getMessages() async {
+  Future<void> getMessages() async {
     var messages = await firestore.collection('messages').getDocuments();
 
     for (var message in messages.documents) {
@@ -58,29 +59,7 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            StreamBuilder<QuerySnapshot>(
-              stream: firestore.collection('messages').snapshots(),
-              builder: (context, snapshot) {
-                final messages = snapshot.data.documents;
-                List<MessageTile> messageStreams = [];
-                for (var message in messages) {
-                  final messageSender = message.data['sender'];
-                  final messageText = message.data['text'];
-
-                  if (messageSender == user.email) {}
-
-                  messageStreams.add(MessageTile(
-                    sender: messageSender.toString().split('@')[0],
-                    text: messageText,
-                  ));
-                }
-                return Expanded(
-                  child: ListView(
-                    children: messageStreams,
-                  ),
-                );
-              },
-            ),
+            MessageStream(firestore: firestore, user: user),
             Container(
               decoration: kMessageContainerDecoration,
               // alignment: Alignment.bottomCenter,
@@ -118,23 +97,75 @@ class _ChatScreenState extends State<ChatScreen> {
   }
 }
 
+class MessageStream extends StatelessWidget {
+  const MessageStream({
+    Key key,
+    @required this.firestore,
+    @required this.user,
+  }) : super(key: key);
+
+  final Firestore firestore;
+  final FirebaseUser user;
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: firestore.collection('messages').snapshots(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return ModalProgressHUD(inAsyncCall: true, child: Center());
+        }
+        final messages = snapshot.data.documents;
+
+        bool isme = false;
+        List<MessageTile> messageStreams = [];
+        for (var message in messages) {
+          final messageSender = message.data['sender'];
+          final messageText = message.data['text'];
+
+          if (messageSender == user.email) {
+            isme = true;
+          }
+
+          messageStreams.add(MessageTile(
+            sender: messageSender.toString().split('@')[0],
+            text: messageText,
+            isme: isme,
+          ));
+        }
+        return Expanded(
+          child: ListView(
+            children: messageStreams,
+          ),
+        );
+      },
+    );
+  }
+}
+
 class MessageTile extends StatelessWidget {
   final text;
   final sender;
+  final bool isme;
 
-  const MessageTile({Key key, this.text, this.sender}) : super(key: key);
+  const MessageTile({
+    Key key,
+    this.text,
+    this.sender,
+    this.isme,
+  }) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Material(
         borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(10),
-          topRight: Radius.circular(10),
-          bottomLeft: Radius.circular(10),
+          topLeft: Radius.circular(25),
+          topRight: Radius.circular(25),
+          bottomLeft: Radius.circular(25),
         ),
         elevation: 12,
-        color: Colors.blueAccent,
+        color: isme ? Colors.blueAccent : Colors.pinkAccent,
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
